a81e475ccbf633f4a2593ccb74daddee6ed66d79
OlgaSh <Olga.Shenya@gmail.com>
2023-01-19 14:31:39 +0300

    create model methods

--- db.js
+++ db.js
@@ -16,20 +16,20 @@ TodoModel = sequelize.define("todo", {
         type: Sequelize.STRING,
         allowNull: false
     },
-    ischecked: {
+    isChecked: {
         type: Sequelize.BOOLEAN,
         allowNull: false
     }
 });
 
-module.exports.CreateTodo = async (userId, text, ischecked) => {
+module.exports.CreateTodo = async (userId, text, isChecked) => {
     const result = {};
     try {
 
         await TodoModel.create(
             {
                 text: text,
-                ischecked: ischecked,
+                isChecked: isChecked,
                 userId: userId
             }
         )
@@ -39,30 +39,60 @@ module.exports.CreateTodo = async (userId, text, ischecked) => {
     return result;
 }
 
-module.exports.GetTodo = async (pageSize, pageNumber, userId, ischecked) => {
+module.exports.GetTodo = async (pageSize, pageNumber, userId, isChecked) => {
     const result = {};
     try {
-        await TodoModel.findAndCountAll({
+        const res = await TodoModel.findAndCountAll({
             order: [['id', 'ASC']],
             limit: pageSize || 5,
             offset: (pageNumber || 0) * (pageSize || 5),
-            where: ischecked === undefined ? { userId: userId } : {
+            where: isChecked === undefined ? { userId: userId } : {
                 userId: userId,
-                ischecked: ischecked
+                isChecked: isChecked
             }
         })
+        result.total = res.count;
+        result.rows = res.rows;
     } catch (error) {
         result.error = error.message;
     }
     return result;
 }
 
-module.exports.UpdateTodo = async (ischecked, text, id) => {
+module.exports.CountTodo = async (userId) => {
+    let res = {
+        active: 0,
+        completed: 0,
+    };
+    try {
+        const counts = await TodoModel.count({
+            group: ['isChecked'],
+            where: {
+                userId: userId
+            }
+        })
+        //[{isChecked:true,count:3},{isChecked:false,count:3}]
+        counts.forEach(item => {
+            if (item.isChecked === null) return;
+            if (item.isChecked) {
+                res.completed = item.count;
+            } else {
+                res.active = item.count;
+            }
+        });
+    } catch (error) {
+        res.error = error.message;
+    }
+    return res;
+}
+
+
+module.exports.UpdateTodo = async (isChecked, text, id) => {
     const result = {};
     try {
         await TodoModel.update(cleanobj({
             text: text,
-            ischecked: ischecked
+            isChecked: isChecked
         }), {
             where: {
                 id: id
@@ -74,11 +104,11 @@ module.exports.UpdateTodo = async (ischecked, text, id) => {
     return result;
 }
 
-module.exports.UpdateTodoIschecked = async (ischecked, userId) => {
+module.exports.UpdateTodoIschecked = async (isChecked, userId) => {
     const result = {};
     try {
         await TodoModel.update({
-            ischecked: ischecked
+            isChecked: isChecked
         }, {
             where: {
                 userId: userId
@@ -111,9 +141,10 @@ module.exports.DeleteTodoChecked = async (userId) => {
         await TodoModel.destroy({
             where: {
                 userId: userId,
-                ischecked: true,
+                isChecked: true,
             }
         })
+
     } catch (error) {
         result.error = error.message;
     }
@@ -153,13 +184,15 @@ module.exports.CreateUser = async (login, password) => {
 module.exports.GetUser = async (login, password) => {
     const result = {};
     try {
-        result = await UserModel.findOne({
+        const user = await UserModel.findOne({
             attributes: ['id', 'login'],
             where: {
                 login: login || "nologin",
                 password: password || "nopassword"
             }
         })
+        result.id = user.id;
+        result.login = user.login;
     } catch (error) {
         result.error = error.message;
     }
@@ -196,7 +229,7 @@ module.exports.UpdateUser = async (user) => {
     return result;
 }
 //********************************************************************************* */
-this.Todo.belongsTo(UserModel, {
+TodoModel.belongsTo(UserModel, {
     onDelete: 'cascade',
     foreignKey: {
         field: 'userId',
